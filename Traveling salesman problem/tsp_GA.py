b3d4ba08ce570ad51459e3c1748f6452b98aca0d
takakazu <t.s.gemini0530@gmail.com>
2019-06-21 15:24:44 +0900

    add plot

--- Traveling salesman problem/tsp_GA.py
+++ Traveling salesman problem/tsp_GA.py
@@ -1,5 +1,7 @@
 ﻿import math
 import random
+import matplotlib.pyplot as plt
+
 
 random.seed(5)
 
@@ -218,66 +220,154 @@ class GA:
 if __name__ == '__main__':
    
    tourmanager = TourManager()
+   x_list = []
+   y_list = []
    
    # Create and add our cities
    city = City(1150, 1760)
    tourmanager.addCity(city)
+   #x_list.append(1150)
+   #y_list.append(1760)
+
    city2 = City(630, 1660)
    tourmanager.addCity(city2)
+   #x_list.append(630)
+   #y_list.append(1660)
+
    city3 = City(40, 2090)
    tourmanager.addCity(city3)
+   #x_list.append(40)
+   #y_list.append(2090)
+
    city4 = City(750, 1100)
    tourmanager.addCity(city4)
+   #x_list.append(750)
+   #y_list.append(1100)
+
    city5 = City(750, 2030)
    tourmanager.addCity(city5)
+   #x_list.append(750)
+   #y_list.append(2030)
+
    city6 = City(1030, 2070)
    tourmanager.addCity(city6)
+   #x_list.append(1030)
+   #y_list.append(2070)
+
    city7 = City(1650, 650)
    tourmanager.addCity(city7)
+   #x_list.append(1650)
+   #y_list.append(650)
+
    city8 = City(1490, 1630)
    tourmanager.addCity(city8)
+   #x_list.append(1490)
+   #y_list.append(1630)
+
    city9 = City(790, 2260)
    tourmanager.addCity(city9)
+   #x_list.append(790)
+   #y_list.append(2260)
+
    city10 = City(710, 1310)
    tourmanager.addCity(city10)
+   #x_list.append(710)
+   #y_list.append(1310)
+
    city11 = City(840, 550)
    tourmanager.addCity(city11)
+   #x_list.append(840)
+   #y_list.append(550)
+
    city12 = City(1170, 2300)
    tourmanager.addCity(city12)
+   #x_list.append(1170)
+   #y_list.append(2300)
+
    city13 = City(970, 1340)
    tourmanager.addCity(city13)
+   #x_list.append(970)
+   #y_list.append(1340)
+
    city14 = City(510, 700)
    tourmanager.addCity(city14)
+   #x_list.append(510)
+   #y_list.append(700)
+
    city15 = City(750, 900)
    tourmanager.addCity(city15)
+   #x_list.append(750)
+   #y_list.append(900)
+
    city16 = City(1280, 1200)
    tourmanager.addCity(city16)
+   #x_list.append(1280)
+   #y_list.append(1200)
+
    city17 = City(230, 590)
    tourmanager.addCity(city17)
+   #x_list.append(230)
+   #y_list.append(590)
+
    city18 = City(460, 860)
    tourmanager.addCity(city18)
+   #x_list.append(460)
+   #y_list.append(860)
+
    city19 = City(1040, 950)
    tourmanager.addCity(city19)
+   #x_list.append(1040)
+   #y_list.append(950)
+
    city20 = City(590, 1390)
    tourmanager.addCity(city20)
+   #x_list.append(590)
+   #y_list.append(1390)
+
    city21 = City(830, 1770)
    tourmanager.addCity(city21)
+   #x_list.append(830)
+   #y_list.append(1770)
+
    city22 = City(490, 500)
    tourmanager.addCity(city22)
+   #x_list.append(490)
+   #y_list.append(500)
+
    city23 = City(1840, 1240)
    tourmanager.addCity(city23)
+   #x_list.append(1840)
+   #y_list.append(1240)
+
    city24 = City(1260, 1500)
    tourmanager.addCity(city24)
+   #x_list.append(1260)
+   #y_list.append(1500)
+
    city25 = City(1280, 790)
    tourmanager.addCity(city25)
+   #x_list.append(1280)
+   #y_list.append(790)
+
    city26 = City(490, 2130)
    tourmanager.addCity(city26)
+   #x_list.append(490)
+   #y_list.append(2130)
+
    city27 = City(1460, 1420)
    tourmanager.addCity(city27)
+   #x_list.append(1460)
+   #y_list.append(1420)
+
    city28 = City(1260, 1910)
    tourmanager.addCity(city28)
+   #x_list.append(1260)
+   #y_list.append(1910)
+
    city29 = City(360, 1980)
    tourmanager.addCity(city29)
+   #x_list.append(360)
+   #y_list.append(1980)
    
    
    pop = Population(tourmanager, 1000, True);
@@ -293,4 +383,74 @@ if __name__ == '__main__':
    print ("Finished")
    print ("Final distance: " + str(pop.getFittest().getDistance()))
    print ("Solution:")
-   print (pop.getFittest())
+
+   print(pop.getFittest())
+   
+   x_list.append(1170)
+   x_list.append(1030)
+   x_list.append(1260)
+   x_list.append(1150)
+   x_list.append(1260)
+   x_list.append(970)
+   x_list.append(1280)
+   x_list.append(1460)
+   x_list.append(1490)
+   x_list.append(1840)
+   x_list.append(1650)
+   x_list.append(1280)
+   x_list.append(1040)
+   x_list.append(840)
+   x_list.append(490)
+   x_list.append(230)
+   x_list.append(510)
+   x_list.append(460)
+   x_list.append(750)
+   x_list.append(750)
+   x_list.append(710)
+   x_list.append(590)
+   x_list.append(630)
+   x_list.append(830)
+   x_list.append(750)
+   x_list.append(360)
+   x_list.append(40)
+   x_list.append(490)
+   x_list.append(790)
+
+   y_list.append(2300)
+   y_list.append(2070)
+   y_list.append(1910)
+   y_list.append(1760)
+   y_list.append(1500)
+   y_list.append(1340)
+   y_list.append(1200)
+   y_list.append(1420)
+   y_list.append(1630)
+   y_list.append(1240)
+   y_list.append(650)
+   y_list.append(790)
+   y_list.append(950)
+   y_list.append(550)
+   y_list.append(500)
+   y_list.append(590)
+   y_list.append(700)
+   y_list.append(860)
+   y_list.append(900)
+   y_list.append(1100)
+   y_list.append(1310)
+   y_list.append(1390)
+   y_list.append(1660)
+   y_list.append(1770)
+   y_list.append(2030)
+   y_list.append(1980)
+   y_list.append(2090)
+   y_list.append(2130)
+   y_list.append(2260)
+
+
+   fig,ax = plt.subplots()
+   data_name=['12','6','28','1','24','13','16','27','8','23','7','25','19','11','22','17','14','18','15','4','10','20','2','21','5','29','3','26','9']
+   ax.scatter(x_list,y_list)
+   plt.plot(x_list,y_list)
+   for i,j,k in zip(x_list,y_list,data_name):
+        ax.annotate(k,(i,j))
+   plt.show()
